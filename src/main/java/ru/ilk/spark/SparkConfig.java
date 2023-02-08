0075afe697fcc6f154120541e9171160aaaa4e3f
MaASemenycheva <41669988+MaASemenycheva@users.noreply.github.com>
2023-02-08 11:27:22 -0500

    Update SparkConfig.java

--- src/main/java/ru/ilk/spark/SparkConfig.java
+++ src/main/java/ru/ilk/spark/SparkConfig.java
@@ -3,6 +3,7 @@ package ru.ilk.spark;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import ru.ilk.cassandra.CassandraConfig;
+import ru.ilk.config.Config;
 
 public class SparkConfig {
     private static Logger logger = Logger.getLogger(SparkConfig.class.getName());
@@ -16,7 +17,16 @@ public class SparkConfig {
     public static Integer batchInterval;
 
     public static void load() {
-
+        logger.info("Loading Spark Setttings");
+        sparkConf.set("spark.streaming.stopGracefullyOnShutdown", Config.applicationConf.getString("config.spark.gracefulShutdown"))
+                .set("spark.sql.streaming.checkpointLocation", Config.applicationConf.getString("config.spark.checkpoint"))
+                .set("spark.cassandra.connection.host", Config.applicationConf.getString("config.cassandra.host"));
+        shutdownMarker = Config.applicationConf.getString("config.spark.shutdownPath");
+        batchInterval = Integer.valueOf(Config.applicationConf.getString("config.spark.batch.interval"));
+        transactionDatasouce = Config.localProjectDir + Config.applicationConf.getString("config.spark.transaction.datasource");
+        customerDatasource = Config.localProjectDir + Config.applicationConf.getString("config.spark.customer.datasource");
+        modelPath = Config.localProjectDir + Config.applicationConf.getString("config.spark.model.path");
+        preprocessingModelPath = Config.localProjectDir + Config.applicationConf.getString("config.spark.model.preprocessing.path");
     }
 
     public static void defaultSetting() {
