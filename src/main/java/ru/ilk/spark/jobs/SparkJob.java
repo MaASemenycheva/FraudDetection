121941c7279b679d60f8c5994fd56b9f0793d45d
MaASemenycheva <41669988+MaASemenycheva@users.noreply.github.com>
2023-02-08 11:26:29 -0500

    Update SparkJob.java

--- src/main/java/ru/ilk/spark/jobs/SparkJob.java
+++ src/main/java/ru/ilk/spark/jobs/SparkJob.java
@@ -3,14 +3,16 @@ package ru.ilk.spark.jobs;
 import org.apache.spark.sql.SparkSession;
 import ru.ilk.spark.SparkConfig;
 
-//abstract class SparkJob{
-public class SparkJob {
+class SparkJob {
     String appNameSparkJob;
+    SparkSession sparkSession;
     public SparkJob(String appName) {
         appNameSparkJob = appName;  // Set the initial value for the class attribute x
+        sparkSession = SparkSession
+                .builder()
+                .appName(appName)
+                .master("local")
+                .config(SparkConfig.sparkConf)
+                .getOrCreate();
     }
-
-    public static SparkSession sparkSession = SparkSession.builder()
-            .config(SparkConfig.sparkConf)
-            .getOrCreate();
 }
