089317849c772af57cead2df762bf5fc826ffe6d
MaASemenycheva <41669988+MaASemenycheva@users.noreply.github.com>
2023-02-21 12:02:23 -0500

    Update DataReader.java

--- src/main/java/ru/ilk/spark/DataReader.java
+++ src/main/java/ru/ilk/spark/DataReader.java
@@ -1,24 +1,16 @@
 package ru.ilk.spark;
 
-//import javafx.util.Pair;
-
 import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-//import org.apache.spark.streaming.kafka010.HasOffsetRanges;
-
 
 public class DataReader {
     private static Logger logger = Logger.getLogger(DataReader.class.getName());
 
     public static Dataset<Row> read(String transactionDatasource,
-//                                    StructType schema,
                                     SparkSession sparkSession) {
         return sparkSession.read()
-//                .option("header", "true")
-//                .schema(schema)
-//                .csv(transactionDatasource);
                 .format("csv")
                 .option("header", "true")
                 .load(transactionDatasource);
@@ -32,15 +24,4 @@ public class DataReader {
                 .option("pushdown", "true")
                 .load();
     }
-//    public static void getOffset (RDD<HasOffsetRanges> rdd, SparkSession sparkSession) {
-//        HasOffsetRanges hasOffsetRanges = (HasOffsetRanges) rdd.toJavaRDD().rdd();
-//
-//        StructType featureSchema = new StructType();
-//        featureSchema.add("partition", DataTypes.StringType, true);
-//        featureSchema.add("offset", DataTypes.StringType, true);
-//
-//        List<Pair<Integer, Long>> rowRdd = (List<Pair<Integer, Long>>) Arrays.stream(hasOffsetRanges.offsetRanges()).map(offset ->
-//                new Pair<Integer, Long>(offset.partition(), offset.untilOffset()));
-//        sparkSession.createDataFrame((JavaRDD<Row>) rowRdd, featureSchema);
-//    }
 }
